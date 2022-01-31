1b7ae6637929962d03843f5ac647558c4c5ec6bf
Manu-Rama <manuelramabernal@gmail.com>
2022-01-31 19:42:17 +0100

    ok

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -22,63 +22,45 @@ const BookForm = () => {
   const [title, setTitle] = useState();
   const [editorial, setEditorial] = useState();
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("submit");
+    // TODO: validar campos
+
+    const data = {
+      titulo: "test",
+      editorial: "hgwaiuehw",
+      precio: "31",
+    };
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    };
+
+    fetch("http://localhost:8000", requestOptions)
+      .then((response) => response.json())
+      .then((json) => console.log(json.results))
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
+  }
+
   return (
     <form
       style={{
         marginTop: "50px",
       }}
-
-      /*onSubmit={}*/
+      action="https://www.nuclio.school"
+      onSubmit={handleSubmit}
     >
       <label>title</label>
       <input
         type="text"
         name="title"
         onChange={(event) => setTitle(event.target.value)}
-    />
-
-    function handleSubmit(event) {
-        event.preventDefault() { 
-            console.log("submit")
-        // TODO: validar campos
-        }
-        
-        
-        const data = {
-            "titulo": "test",
-            "editorial": "hgwaiuehw",
-            "precio": "31"
-        }
-
-        const requestOptions = {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(data)
-        }
-
-        fetch("http://localhost:8000", requestOptions) {
-            .then((response) => response.json())
-            .then((json) => console.log(json.results))
-            .catch((error) => {
-                console.error('Error: ', error)
-            })
-        }    
-    
-
-    return (
-        <form
-            style = {{
-                marginTop: "50px"
-            }
-        }
-
-        action="https://www.nuclio.school"
-
-        onSubmit={handleSubmit}
-        >
-            <label>title</label>
-            <input type="text" name="title" onChange={(event) => setTitle(event.target.value)} />
-
+      />
 
       <label>editorial</label>
       <input
