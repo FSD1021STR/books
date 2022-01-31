8977485b84e047370a24bac7d39832bfb7141a40
Jordi Galobart Salvat <jgalobart@gmail.com>
2022-01-31 19:06:23 +0100

    third commit

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -25,6 +25,31 @@ const BookForm = () => {
     const [title,setTitle] = useState()
     const [editorial,setEditorial] = useState()
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("submit");
+        // TODO: validar campos
+        
+        const data = {
+            "titulo": "test",
+            "editorial": "hgwaiuehw",
+            "precio": "31"
+        }
+
+        const requestOptions = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(data)
+        }
+
+        fetch("http://localhost:8000", requestOptions)
+            .then((response) => response.json())
+            .then((json) => console.log(json.results))
+            .catch((error) => {
+                console.error('Error: ', error)
+            })
+    }
+
     return (
         <form
             style = {{
@@ -32,7 +57,9 @@ const BookForm = () => {
             }
         }
 
-        /*onSubmit={}*/
+        action="https://www.nuclio.school"
+
+        onSubmit={handleSubmit}
         >
             <label>title</label>
             <input type="text" name="title" onChange={(event) => setTitle(event.target.value)} />
