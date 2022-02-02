f5c30edb7e4f80a9151ce6c6b540cf0d52079aa1
Jordi Galobart Salvat <jgalobart@gmail.com>
2022-02-02 16:05:31 +0100

    add contexts

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,40 +1,85 @@
-import React, {useState} from "react";
+import React, {useState,useContext} from "react";
 
 const BookForm = () => {
 
-    
-        /*
-        const data = {
-            "titulo": "test",
-            "editorial": "hgwaiuehw",
-            "precio": "31"
+    console.log(process.env.REACT_APP_ENVIRONMENT)
+
+    const environments = {
+        'dev': {
+            'api': 'http://localhost:8000',
+            'limit': 3,
+            'theme': 'dark',
+            'debug': true,
+            'payments': false,
+        },
+        'sandbox': {
+            'api': 'https://test.myapp.com',
+            'limit': 10,
+            'theme': 'pink',
+            'debug': true
+        },
+        'prod': {
+            'api': 'https://www.myapp.com',
+            'limit': 10,
+            'theme': 'light',
+            'debug': false
         }
+    }
 
-        const requestOptions = {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(data)
+    const themes = {
+        'light': {
+            'text': 'black',
+            'background': 'white'
+        },
+        'dark': {
+            'text': 'white',
+            'background': 'black'
         }
+    }
 
-        fetch("http://localhost:8000", requestOptions)
-            .then((response) => response.json())
-            .then((json) => setTypes(json.results))
-        */
+    var EnvironmentContext = ""
+
+    console.log("hola");
+
+    if (process.env.REACT_APP_ENVIRONMENT === 'prod'){
+        EnvironmentContext = React.createContext(environments.prod)
+    }
+        
+    else if (process.env.REACT_APP_ENVIRONMENT === 'sandbox') {
+        EnvironmentContext = React.createContext(environments.sandbox)
+    }
+    else {
+        EnvironmentContext = React.createContext(environments.sandbox)
+    }
+
+    const environment = useContext(EnvironmentContext)
     
 
-    const [title,setTitle] = useState()
-    const [editorial,setEditorial] = useState()
+    console.log ('%%%%%%%%')
+    console.info (environment.api)
+    console.info (environment.limit)
+    console.info (environment.theme)
+    console.info (environment.debug)
+    console.log ('%%%%%%%%')
+
+    const ThemeContext = React.createContext(themes.light)
+    const theme = useContext(ThemeContext)
+    console.log('text: '+theme.text)
+    console.log('background: '+theme.background)
+
+    console.log(themes.light)
+
+    const [data,setData] = useState({
+        'title':'',
+        'editorial':'',
+    })
 
     function handleSubmit(event) {
         event.preventDefault();
         console.log("submit");
         // TODO: validar campos
         
-        const data = {
-            "titulo": "test",
-            "editorial": "hgwaiuehw",
-            "precio": "31"
-        }
+        
 
         const requestOptions = {
             method: "POST",
@@ -62,10 +107,10 @@ const BookForm = () => {
         onSubmit={handleSubmit}
         >
             <label>title</label>
-            <input type="text" name="title" onChange={(event) => setTitle(event.target.value)} />
+            <input type="text" name="title" onChange={(event) => setData({...data,'title':event.target.value})} />
 
             <label>editorial</label>
-            <input type="text" name="editorial" onChange={(event) => setEditorial(event.target.value)} />
+            <input type="text" name="editorial" onChange={(event) => setData({...data,'editorial':event.target.value})} />
 
             <button type="submit">Enviar</button>
 
